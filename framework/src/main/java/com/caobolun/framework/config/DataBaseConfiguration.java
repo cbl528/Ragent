4d8452ed274b5c5bcedb59f16e6380c74bd6392d
cbl <cao_bolun@qq.com>
2026-06-23 14:13:01 +0800

    新增数据库配置类

---
+++ framework/src/main/java/com/caobolun/framework/config/DataBaseConfiguration.java
@@ -0,0 +1,27 @@
+package com.caobolun.framework.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.caobolun.framework.database.MyMetaObjectHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataBaseConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.POSTGRE_SQL));
+        return mybatisPlusInterceptor;
+    }
+
+    @Bean
+    public MetaObjectHandler myMetaObjectHandler(){
+        return new MyMetaObjectHandler();
+    }
+
+
+}
