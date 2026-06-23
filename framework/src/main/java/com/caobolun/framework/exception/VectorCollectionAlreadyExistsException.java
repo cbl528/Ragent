c27506f25adc762eb3c95ee9cf348ee29aa9ac54
cbl <cao_bolun@qq.com>
2026-06-23 14:50:06 +0800

    新增向量表重复异常

---
+++ framework/src/main/java/com/caobolun/framework/exception/VectorCollectionAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package com.caobolun.framework.exception;
+
+/**
+ * 向量表重复创建异常
+ */
+public class VectorCollectionAlreadyExistsException extends ServiceException {
+
+    public VectorCollectionAlreadyExistsException(String collectionName) {
+        super("向量集合已存在，禁止重复创建：" + collectionName);
+    }
+}
