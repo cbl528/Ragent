a1d30994f47777795e1a57fd9bdd5bc79bc2efab
cbl <cao_bolun@qq.com>
2026-06-28 14:58:22 +0800

    新增对话控制接口和对话业务对象

---
+++ bootstrap/src/main/java/com/caobolun/bootstrap/rag/service/ConversationServiceImpl.java
@@ -0,0 +1,38 @@
+package com.caobolun.bootstrap.rag.service;
+
+import com.caobolun.bootstrap.rag.dto.request.ConversationUpdateRequest;
+import com.caobolun.bootstrap.rag.dto.vo.ConversationVO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 会话服务实现类
+ * 处理会话的创建、更新、重命名和删除等业务逻辑
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ConversationServiceImpl implements ConversationService {
+    @Override
+    public List<ConversationVO> listByUserId(String userId) {
+        return List.of();
+    }
+
+    @Override
+    public void createOrUpdate(ConversationCreateBO request) {
+
+    }
+
+    @Override
+    public void rename(String conversationId, ConversationUpdateRequest request) {
+
+    }
+
+    @Override
+    public void delete(String conversationId) {
+
+    }
+}
